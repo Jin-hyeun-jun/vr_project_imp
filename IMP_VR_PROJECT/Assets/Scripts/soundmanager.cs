5802d7235fbe3178e3e8714419f0d0b945bf5e8d
Jin-hyeun-jun <yong5jin@ajou.ac.kr>
2022-06-14 01:06:21 +0900

    종료기능추가
    종료기능추가

--- IMP_VR_PROJECT/Assets/Scripts/soundmanager.cs
+++ IMP_VR_PROJECT/Assets/Scripts/soundmanager.cs
@@ -6,12 +6,22 @@ public class soundmanager : MonoBehaviour
 {
 
     public AudioSource endingsound;
+    public float endtime;
     // Start is called before the first frame update
     void Start()
     {
         endingsound.Play();
+        Invoke("end", endtime);
     }
 
+    void end()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
     // Update is called once per frame
-   
+
 }
--- IMP_VR_PROJECT/Assets/Scripts/timeattack.cs
+++ IMP_VR_PROJECT/Assets/Scripts/timeattack.cs
@@ -6,34 +6,24 @@ using UnityEngine.SceneManagement;
 public class timeattack : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float limittime=60.0f;
+    public float limittime=60f;
     public AudioSource witchvoice;
 
-  
 
+    int i = 0;
     // Update is called once per frame
     void Update()
     {
         limittime -= Time.deltaTime;
-        if(limittime==30.0f)
+        if (limittime < 17f && i == 0)
         {
-            Debug.Log("30");
+           
             witchvoice.Play();
+            i++;
         }
-        if (limittime == 15.0f)
-        {
-            witchvoice.Play();
-        }
-        if (limittime == 10.0f)
-        {
-            witchvoice.Play();
-        }
-        if (limittime == 5.0f)
-        {
-            witchvoice.Play();
-        }
+       
 
-        if (limittime==0f)
+        if (limittime<0)
         {
             SceneManager.LoadScene("Ending_Fail");
         }
