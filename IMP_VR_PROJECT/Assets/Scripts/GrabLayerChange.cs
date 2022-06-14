4ff4b2a2c1ca06ebe7445e230013d9ff239de2e8
Hayoung1224 <hayoung200585@gmail.com>
2022-06-14 23:09:39 +0900

    Modify Collision detection with grab objects

--- IMP_VR_PROJECT/Assets/Scripts/GrabLayerChange.cs
+++ IMP_VR_PROJECT/Assets/Scripts/GrabLayerChange.cs
@@ -5,11 +5,31 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class GrabLayerChange : MonoBehaviour
 {
-
-    public void ChangeLayer(SelectEnterEventArgs args)
+    public void ChangeToGrabLayer(SelectEnterEventArgs args)
     {
         int Layer = LayerMask.NameToLayer("Grab");
         gameObject.layer = Layer;
+        if (gameObject.transform.childCount > 0)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.layer = Layer;
+            }
+        }
+        Debug.Log("Current layer: " + gameObject.layer);
+    }
+
+    public void ChangeToDefaultLayer(SelectExitEventArgs args)
+    {
+        int Layer = LayerMask.NameToLayer("Default");
+        gameObject.layer = Layer;
+        if (gameObject.transform.childCount > 0)
+        {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.layer = Layer;
+            }
+        }
         Debug.Log("Current layer: " + gameObject.layer);
     }
 }
